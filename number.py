410ebba30474cd7515878085f7480d5a6b652256
Jeff Davis <alienratt@gmail.com>
2021-06-13 17:06:31 -0500

    Input validation

--- number.py
+++ number.py
@@ -6,11 +6,18 @@ theNumber = random.randint(lowerBound, upperBound)
 guess = 0
 
 while guess != theNumber:
-	guess = int(input("Please guess a number between " + str(lowerBound) + " and " + str(upperBound) + ": "))
-	if guess > theNumber:
-		print("Too high.  Please try again.")
+	validInput = True
+	try:
+		guess = int(input("Please guess a number between " + str(lowerBound) + " and " + str(upperBound) + ": "))
+	except ValueError:
+		validInput = False
+	if validInput:
+		if guess > theNumber:
+			print("Too high.  Please try again.")
+		else:
+			if guess < theNumber:
+				print("Too Low.  Please try again.")
 	else:
-		if guess < theNumber:
-			print("Too Low.  Please try again.")
+		print("The guess wasn't a number. Please enter a number and try again.")
 
 print(theNumber, "is correct!  You Win!")
